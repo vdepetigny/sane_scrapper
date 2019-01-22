1842b6194dcc17e1455440ec39ff46c15052b332
vdepetigny <vdepetigny@yahoo.fr>
2019-01-22 14:58:05 +0100

    enregistrement JSON

--- lib/app/scrapper.rb
+++ lib/app/scrapper.rb
@@ -1,6 +1,7 @@
 # require 'rubygems'
 # require 'nokogiri'
 # require 'open-uri'
+#require 'json'
 
 class Scrapper
 	attr_accessor :url_dept, :urls_town, :email_town
@@ -8,7 +9,7 @@ class Scrapper
 	def initialize(url_dept)
 		@url_dept = url_dept
 		@urls_town = []
-		@email_town = []
+		@email_town = {}
 	end 
 
 	#1 Première méthode : Collecte de l'email d'une mairie d'une ville du Val d'Oise
@@ -18,16 +19,15 @@ class Scrapper
 		email = page.xpath('//*[contains(text(), "@")]').text
 		town = page.xpath('//*[contains(text(), "Adresse mairie de")]').text.split #/ on divise la string pour pouvoir récupérer uniquement le nom de la ville
 
-		@email_town << {town[3] => email} #/ on indique la position du nom de la ville dans la string pour la récupérer
-		puts @email_town
-		return @email_town
+		#@email_town << {town[3] => email} #/ on indique la position du nom de la ville dans la string pour la récupérer
+		@email_town[town[3]] = email #/ on indique la position du nom de la ville dans la string pour la récupérer
+		@email_town
 	end
 
 	#2 Deuxième méthode : Collecte de toutes les URLs des villes du Val d'Oise
 	def get_townhall_urls
 		page = Nokogiri::HTML(open(@url_dept))
 		
-
 		urls = page.xpath('//*[@class="lientxt"]/@href') #/ toutes les URLs appartiennent à la classe lientxt
 
 		urls.each do |url| #/ pour chaque URLs récupérées, il faut leur indiquer l'url parent "http://annuaire-des-mairies.com"
@@ -37,13 +37,32 @@ class Scrapper
 		return @urls_town
 	end
 
-	#3 Troisième méthode : Synchronisation des noms des villes et des emails des mairies
+	def save_as_JSON
+
+			File.open("db/emails.json","w") do |email|
+  				@email_town.each do |mail|
+  					email.write(mail.to_json + "\n" )
+  				end
+			end	
+	end
+
+	def save_as_Google_sheet
+
+			File.open("db/emails.json","w") do |email|
+  				@email_town.each do |mail|
+  					email.write(mail.to_json + "\n" )
+  				end
+			end	
+	end
+	
 	def perform
 		get_townhall_urls
+
 		@urls_town.each do |townhall_url| #/ pour chaque URL d'une ville du Val d'Oise, on associe l'adresse mail de la mairie
 			get_townhall_email(townhall_url)
 		end
+
+		save_as_JSON
 	end
-
-
 end
+
